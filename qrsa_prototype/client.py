d64e7588b927b62ab116ad2e883b1b2677c031bb
Chibikuri <ryosuke.satoh.wk@gmail.com>
2023-08-10 13:23:20 +0900

    introduced async for handling multiple connections efficiently

---
+++ qrsa_prototype/client.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+class QRSAClient:
+
+    def __init__(self, server):
+        pass
--- qrsa_prototype/common/src/common/models/connection_setup_request.py
+++ qrsa_prototype/common/src/common/models/connection_setup_request.py
@@ -1,9 +1,11 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, NewType
 from application_performance_request import ApplicationPerformanceRequest
+
+ApplicationId = NewType("ApplicationId", str)
 
 
 class ConnectionSetupRequest(BaseModel):
-    application_id: str = Field(
+    application_id: ApplicationId = Field(
         ..., alias="Application Id which while connection id is not ready"
     )
     application_performance_request: ApplicationPerformanceRequest = Field(
