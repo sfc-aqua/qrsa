60ed2ba7feb3aeb1c071dd2f5707182c499d4355
Chibikuri <ryosuke.satoh.wk@gmail.com>
2023-08-14 15:35:20 +0900

    updated docker configs

--- qrsa_prototype/.entrypoints/.controller_entrypoint.sh
+++ qrsa_prototype/.entrypoints/.controller_entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 # work dir /usr/app
+# Since the current volume mount shares .venv over the different containers, rye returns
+# installation error. Instead of using rye in the container, we use venv directly.
+python3 -m venv .venv
+. .venv/bin/activate
 pip3 install -r requirements.lock
-python server.py
+python3 server.py
+
--- qrsa_prototype/.entrypoints/.qnode_entrypoint.sh
+++ qrsa_prototype/.entrypoints/.qnode_entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 # work dir /usr/app
+python3 -m venv .venv
+. .venv/bin/activate
 pip3 install -r requirements.lock
-python server.py
+python3 server.py
